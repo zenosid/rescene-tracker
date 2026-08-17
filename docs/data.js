254a8eb937ebfbe5ae07e2ecaab8cd9c2867e411
zenosid <zenosid1@gmail.com>
2026-08-17 18:41:09 +0900

    backfill full youtube history

--- docs/data.js
+++ docs/data.js
@@ -1,5 +1,5 @@
 const SITE_DATA = {
-  "generated_at": "2026-08-17 18:10",
+  "generated_at": "2026-08-17 18:41",
   "operator_contact": "네이버 카페 '리시안셔스' '첸드' 쪽지",
   "refresh_interval_minutes": 30,
   "archive": [
