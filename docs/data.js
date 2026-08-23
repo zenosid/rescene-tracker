44e2ce7cbaf317505db40e69f5a375a3b5bc1300
zenosid <zenosid1@gmail.com>
2026-08-23 22:42:47 +0900

    rebuild data.js

--- docs/data.js
+++ docs/data.js
@@ -1,5 +1,5 @@
 const SITE_DATA = {
-  "generated_at": "2026-08-23 22:23",
+  "generated_at": "2026-08-23 22:42",
   "operator_contact": "네이버 카페 '리시안셔스' '첸드' 쪽지",
   "refresh_interval_minutes": 30,
   "archive": [
@@ -177788,6 +177788,11 @@ const SITE_DATA = {
       "broadcaster": "SBS",
       "name": "러브FM",
       "sms_number": "1035"
+    },
+    {
+      "broadcaster": "EBS",
+      "name": "FM",
+      "sms_number": "1045"
     }
   ]
 };
